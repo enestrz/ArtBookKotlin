6ced5d55c10762a4177e1590bd42edfc2d9b8de6
Enes Terzi <enestrz15@gmail.com>
2024-04-29 03:32:07 +0300

    Art activity page added and designed.

---
+++ app/src/main/java/com/enesterzi/artbookkotlin/ArtActivity.kt
@@ -0,0 +1,20 @@
+package com.enesterzi.artbookkotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ArtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_art)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
