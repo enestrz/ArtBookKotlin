300b771e49761d8b37dd5ab5bf1fbf5efffe8f27
Enes Terzi <enestrz15@gmail.com>
2024-04-29 19:07:35 +0300

    Permission for image gallery implemented.

--- app/src/main/java/com/enesterzi/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/enesterzi/artbookkotlin/ArtActivity.kt
@@ -1,20 +1,116 @@
 package com.enesterzi.artbookkotlin
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.enesterzi.artbookkotlin.databinding.ActivityArtBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ArtActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityArtBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedBitmap: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_art)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        enableEdgeToEdge()
+        binding = ActivityArtBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        registerLauncher()
+    }
+
+    fun saveButtonClicked(view: View) {
+
+    }
+
+    fun selectImage(view: View) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // rationale
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Give Permission", View.OnClickListener {
+                    // request permission
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                })
+                    .show()
+            } else {
+                // request permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+        } else {
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            // intent
+            activityResultLauncher.launch(intentToGallery)
         }
+    }
+
+    private fun registerLauncher() {
+
+        // Görseli seçmek
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+                if(intentFromResult != null) {
+                    val imageData = intentFromResult.data // URI
+                    //binding.imageView.setImageURI(imageData)
+                    if (imageData != null) {
+                        try {
+                            // Eğer SDK versiyonu 28'den büyükse bunu yapıyoruz
+                            if (Build.VERSION.SDK_INT >= 28){
+                                val source = ImageDecoder.createSource(this@ArtActivity.contentResolver, imageData)
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+                            // SDK versiyonu 28'den küçükse bunu yapıyoruz
+                            else {
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageData)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            } else {
+                //permission denied
+                Toast.makeText(this@ArtActivity, "Permission needed!", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/enesterzi/artbookkotlin/MainActivity.kt
+++ app/src/main/java/com/enesterzi/artbookkotlin/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.enesterzi.artbookkotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.enesterzi.artbookkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+//        setTitle("Enes")
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        // inflater
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_art_item){
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+            startActivity(intent)
         }
+
+        return super.onOptionsItemSelected(item)
     }
 }
